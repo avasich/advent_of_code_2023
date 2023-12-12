f25c6b31cbf7cb471804a8a258d8a870a70a1664
Artyom Vasich <vasich.dev@gmail.com>
2023-12-12 22:07:02 +0400

    day 02 part 1

---
+++ src/d02_cube_conundrun.rs
@@ -0,0 +1,46 @@
+fn parse_game_line(line: &str) -> (usize, usize, usize, usize) {
+    let (game_id, attempts) = line.split_once(':').unwrap();
+    let game_id = game_id.split_once(' ').unwrap().1.parse().unwrap();
+
+    let (r, g, b) = attempts
+        .split(';')
+        .flat_map(|attempt| attempt.split(','))
+        .flat_map(|res| res.trim().split_once(' '))
+        .map(|(n, color)| (n.parse().unwrap(), color))
+        .fold((0, 0, 0), |(r, g, b), (n, color)| match color {
+            "red" => (r.max(n), g, b),
+            "green" => (r, g.max(n), b),
+            "blue" => (r, g, b.max(n)),
+            _ => unreachable!(),
+        });
+
+    (game_id, r, g, b)
+}
+
+pub fn possible_games_sum(filename: &str, contents: (usize, usize, usize)) -> usize {
+    crate::utils::read_lines(filename)
+        .map(|line| parse_game_line(&line))
+        .filter(|&(_, r, g, b)| r <= contents.0 && g <= contents.1 && b <= contents.2)
+        .map(|res| res.0)
+        .sum()
+}
+
+#[cfg(test)]
+mod d02_tests {
+    use super::*;
+
+    static EXAMPLE_01: &str = "./inputs/day_02/example_01.txt";
+    static TASK: &str = "./inputs/day_02/task.txt";
+
+    #[test]
+    fn p1_example_test() {
+        let res = possible_games_sum(EXAMPLE_01, (12, 13, 14));
+        assert_eq!(res, 8);
+    }
+
+    #[test]
+    fn p1_task_test() {
+        let res = possible_games_sum(TASK, (12, 13, 14));
+        println!("{res}");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod d01_trebuchet;
+pub mod d02_cube_conundrun;
 mod utils;
