6b763e81322f0644d281efab4997a5e0e3615dbb
Artyom Vasich <vasich.dev@gmail.com>
2023-12-13 22:25:49 +0400

    runner update

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,17 @@ fn main() {
 fn run_day(day_number: usize) {
     use advent_of_code_2023::{utils::Solution, *};
 
-    let d01 = d01_trebuchet::solution();
-    let d02 = d02_cube_conundrum::solution();
-    let d03 = d03_gear_ratios::solution();
-    let d04 = d04_scratchcards::solution();
-    let d05 = d05_fertilizer::solution();
-    let d06 = d06_wait_for_it::solution();
-    let d07 = d07_camel_cards::solution();
+    let solution: Box<dyn Solution> = match day_number {
+        1 => Box::new(d01_trebuchet::solution()),
+        2 => Box::new(d02_cube_conundrum::solution()),
+        3 => Box::new(d03_gear_ratios::solution()),
+        4 => Box::new(d04_scratchcards::solution()),
+        5 => Box::new(d05_fertilizer::solution()),
+        6 => Box::new(d06_wait_for_it::solution()),
+        7 => Box::new(d07_camel_cards::solution()),
+        _ => unreachable!(),
+    };
 
-    let days: Vec<&dyn Solution> = vec![&d01, &d02, &d03, &d04, &d05, &d06, &d07];
-
-    let day = days[day_number - 1];
-    day.run_part_1();
-    day.run_part_2();
+    solution.run_part_1();
+    solution.run_part_2();
 }
