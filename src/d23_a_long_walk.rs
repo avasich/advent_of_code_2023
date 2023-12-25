b4decd2e20a07e1cc49c9b9bf94ca39e6190958b
Artyom Vasich <vasich.dev@gmail.com>
2023-12-25 17:37:53 +0400

    day 23 part 2 very slow

--- src/d23_a_long_walk.rs
+++ src/d23_a_long_walk.rs
@@ -1,4 +1,4 @@
-use std::fmt::Formatter;
+use std::{cell::RefCell, collections::HashSet, fmt::Formatter, rc::Rc};
 
 use itertools::Itertools;
 
@@ -60,6 +60,32 @@ impl std::fmt::Display for Tile {
     }
 }
 
+struct Node {
+    xy: (usize, usize),
+    prev: Option<Rc<RefCell<Node>>>,
+    children: usize,
+}
+
+impl Node {
+    fn new(xy: (usize, usize)) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Self {
+            xy,
+            prev: None,
+            children: 0,
+        }))
+    }
+
+    fn attach(xy: (usize, usize), other: &Rc<RefCell<Self>>) -> Rc<RefCell<Self>> {
+        let node = Self::new(xy);
+        node.borrow_mut().prev = Some(Rc::clone(other));
+        node
+    }
+
+    fn remove_child(&mut self) {
+        self.children -= 1;
+    }
+}
+
 struct Labyrinth {
     tiles: Vec<Tile>,
     w: usize,
@@ -118,23 +144,87 @@ impl Labyrinth {
             .max()
     }
 
+    fn traverse_ignore_slope(&mut self) -> usize {
+        let mut res = 0;
+
+        let mut stack = vec![(Node::new(self.start), 0)];
+        let mut visited = HashSet::new();
+
+        fn unwind(mut node: Rc<RefCell<Node>>, visited: &mut HashSet<(usize, usize)>) {
+            while node.borrow().children == 0 {
+                visited.remove(&node.borrow().xy);
+                match &node.clone().borrow().prev {
+                    None => break,
+                    Some(prev) => {
+                        prev.borrow_mut().remove_child();
+                        node = prev.clone();
+                    }
+                }
+            }
+        }
+
+        while let Some((node, len)) = stack.pop() {
+            let (x, y) = node.borrow().xy;
+            if (x, y) == self.exit {
+                if len > res {
+                    res = len;
+                }
+                unwind(node, &mut visited);
+                continue;
+            }
+
+            let to_check = [Dir::U, Dir::L, Dir::D, Dir::R]
+                .into_iter()
+                .flat_map(|dir| self.try_step_ignore_slope(x, y, dir))
+                .filter(|next| !visited.contains(next))
+                .collect_vec();
+
+            if to_check.is_empty() {
+                unwind(node, &mut visited);
+            } else {
+                visited.insert((x, y));
+                node.borrow_mut().children += to_check.len();
+                stack.extend(
+                    to_check
+                        .into_iter()
+                        .map(|next| Node::attach(next, &node))
+                        .map(|next| (next, len + 1)),
+                )
+            }
+        }
+
+        res
+    }
+
     fn try_step(&self, x: usize, y: usize, dir: Dir) -> Option<(usize, usize)> {
         match (dir, self.get(x, y)) {
-            (Dir::U, Tile::E | Tile::U) if y > 0 => Some((x, y - 1)),
-            (Dir::U, _) => None,
-            (Dir::L, Tile::E | Tile::L) if x > 0 => Some((x - 1, y)),
-            (Dir::L, _) => None,
-            (Dir::D, Tile::E | Tile::D) if y + 1 < self.h => Some((x, y + 1)),
-            (Dir::D, _) => None,
-            (Dir::R, Tile::E | Tile::R) if x + 1 < self.w => Some((x + 1, y)),
-            (Dir::R, _) => None,
+            (Dir::U, Tile::U | Tile::E) if y > 0 => Some((x, y - 1)),
+            (Dir::L, Tile::L | Tile::E) if x > 0 => Some((x - 1, y)),
+            (Dir::D, Tile::D | Tile::E) if y + 1 < self.h => Some((x, y + 1)),
+            (Dir::R, Tile::R | Tile::E) if x + 1 < self.w => Some((x + 1, y)),
+            _ => None,
         }
         .filter(|&(x, y)| self.get(x, y).is_walkable())
     }
 
-    fn longest_path(&mut self) -> Option<usize> {
+    fn try_step_ignore_slope(&self, x: usize, y: usize, dir: Dir) -> Option<(usize, usize)> {
+        match dir {
+            Dir::U if y > 0 => Some((x, y - 1)),
+            Dir::L if x > 0 => Some((x - 1, y)),
+            Dir::D if y + 1 < self.h => Some((x, y + 1)),
+            Dir::R if x + 1 < self.w => Some((x + 1, y)),
+            _ => None,
+        }
+        .filter(|&(x, y)| self.get(x, y).is_walkable())
+    }
+
+    fn longest_path(&mut self, ignore_slope: bool) -> usize {
         let (x, y) = self.start;
-        self.traverse(x, y, 0)
+        if ignore_slope {
+            self.traverse_ignore_slope()
+        } else {
+            self.traverse(x, y, 0).unwrap()
+        }
     }
 
     fn get(&self, x: usize, y: usize) -> Tile {
@@ -155,16 +245,14 @@ impl Labyrinth {
 }
 
 fn part_1(filename: &str) -> usize {
-    let mut labyrinth = Labyrinth::from_file(filename);
-    let res = labyrinth.longest_path();
-    res.unwrap()
+    Labyrinth::from_file(filename).longest_path(false)
 }
 
-fn part_2(filename: &str) -> u64 {
-    todo!()
+fn part_2(filename: &str) -> usize {
+    Labyrinth::from_file(filename).longest_path(true)
 }
 
-pub fn solution() -> Day<usize, u64> {
+pub fn solution() -> Day<usize, usize> {
     Day {
         part_1: Task {
             examples: vec!["./inputs/day_23/example_01.txt"],
@@ -180,7 +268,7 @@ pub fn solution() -> Day<usize, u64> {
 }
 
 #[cfg(test)]
-mod d20_tests {
+mod d23_tests {
     use super::*;
 
     #[test]
@@ -194,6 +282,6 @@ mod d20_tests {
     fn p2_example_test() {
         let solution = solution();
         let res = solution.part_2.run_example(0);
-        assert_eq!(0, res);
+        assert_eq!(154, res);
     }
 }
